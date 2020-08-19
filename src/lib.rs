1063826af4e75dcc1701dedd08db49c3f5ecb22d
Soveu <marx.tomasz@gmail.com>
2020-08-19 12:51:25 +0200

    polishing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 pub mod stacc;
 pub mod spsc_queue;
 pub mod stacc_lockfree;
-pub mod atomic_arc;
 
--- src/stacc_lockfree.rs
+++ src/stacc_lockfree.rs
@@ -6,6 +6,12 @@ use std::sync::{
     atomic::*,
 };
 
+/* NonNull must come from Box::into_raw */
+unsafe fn nonnull_to_box<T>(ptr: NonNull<StaccNode<T>>) -> Box<StaccNode<T>> {
+    assert_eq!(ptr.as_ref().counter.load(Ordering::Acquire), 0);
+    return Box::from_raw(ptr.as_ptr());
+}
+
 struct StaccNode<T> {
     next: Option<NonNull<StaccNode<T>>>,
     counter: AtomicUsize,
@@ -28,19 +34,27 @@ struct StaccInner<T> {
     global_garbage: Mutex<Vec<NonNull<StaccNode<T>>>>,
 }
 
-unsafe fn nonnull_to_box<T>(ptr: NonNull<StaccNode<T>>) -> Box<StaccNode<T>> {
-    assert_eq!(ptr.as_ref().counter.load(Ordering::Acquire), 0);
-    return Box::from_raw(ptr.as_ptr());
+impl<T> StaccInner<T> {
+    fn new() -> Self {
+        Self {
+            head: AtomicPtr::new(0 as *mut _),
+            len: AtomicUsize::new(0),
+            global_garbage: Mutex::new(vec![]),
+        }
+    }
 }
 
 impl<T> Drop for StaccInner<T> {
     fn drop(&mut self) {
         let garbage = self.global_garbage.get_mut().unwrap();
+
+        /* SAFETY: We should be the only one having access to allocated memory */
         garbage.iter()
             .copied()
             .map(|p| unsafe { nonnull_to_box(p) })
             .for_each(drop);
 
+        /* SAFETY: We should be the only one having access to allocated memory */
         while let Some(ptr) = self.pop() {
             let mut boxed = unsafe { nonnull_to_box(ptr) };
             unsafe { ptr::drop_in_place(boxed.item.as_mut_ptr()) };
@@ -54,7 +68,10 @@ impl<T> StaccInner<T> {
         loop {
             let head = self.head.load(Ordering::Acquire);
             let head = NonNull::new(head)?;
+
+            /* SAFETY: head is non-null, so it should be pointing to right element */
             let headref = unsafe { head.as_ref() };
+
             headref.counter.fetch_add(1, Ordering::Relaxed);
             let newhead = match headref.next {
                 None => 0 as *mut _,
@@ -88,6 +105,7 @@ impl<T> StaccInner<T> {
             Ordering::Acquire,
             Ordering::Relaxed)
         {
+            /* SAFETY: we own the allocated object, so it must still exist */
             unsafe { (*node).next = NonNull::new(newhead) };
         }
 
@@ -101,8 +119,16 @@ pub struct Stacc<T> {
 }
 
 impl<T> Stacc<T> {
+    pub fn new() -> Self {
+        Self {
+            inner: Arc::new(StaccInner::new()),
+            local_garbage: vec![],
+        }
+    }
+
     fn make_node(&mut self, x: T) -> Box<StaccNode<T>> {
         if let Some(nonnull) = self.local_garbage.pop() {
+            /* SAFETY: local_garbage should have only pointers that come from Box::into_raw */
             unsafe {
                 let mut boxed = Box::from_raw(nonnull.as_ptr());
                 boxed.item = MaybeUninit::new(x);
@@ -114,7 +140,10 @@ impl<T> Stacc<T> {
     }
     pub fn pop(&mut self) -> Option<T> {
         let nonnull = self.inner.pop()?;
+
+        /* SAFETY: `pop()?` should give us only valid pointers */
         let item = unsafe {
+            nonnull.as_ref().counter.fetch_sub(1, Ordering::Relaxed);
             ptr::read(&nonnull.as_ref().item).assume_init()
         };
 
@@ -132,20 +161,30 @@ impl<T> Stacc<T> {
     }
 }
 
+impl<T> Clone for Stacc<T> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: Arc::clone(&self.inner),
+            local_garbage: Vec::new(),
+        }
+    }
+}
+
 impl<T> Drop for Stacc<T> {
     fn drop(&mut self) {
+        let mut lock = self.inner.global_garbage.lock().unwrap();
         for ptr in self.local_garbage.iter().copied() {
+            /* SAFETY: the pointers from local_garbage should be valid */
             let counter = unsafe { ptr.as_ref().counter.load(Ordering::Acquire) };
 
             if counter == 0 {
+                /* SAFETY: the pointers from local_garbage should come from Box::into_raw */
                 let boxed = unsafe { Box::from_raw(ptr.as_ptr()) };
                 drop(boxed);
                 continue;
             }
 
-            let mut lock = self.inner.global_garbage.lock().unwrap();
             lock.push(ptr);
-            drop(lock);
         }
     }
 }
