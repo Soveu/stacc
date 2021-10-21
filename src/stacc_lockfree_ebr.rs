b15d4f85598a023dc00bd582f2a330db82c3396c
Soveu <marx.tomasz@gmail.com>
2021-10-21 21:08:34 +0200

    i got nerd-sniped again

--- src/stacc_lockfree_ebr.rs
+++ src/stacc_lockfree_ebr.rs
@@ -1,9 +1,11 @@
-use std::sync::atomic::{AtomicU8, AtomicUsize, Ordering};
+use std::sync::atomic::{fence, AtomicU8, AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
 
 const MAX_THREADS: usize = 32;
 
 pub struct Shared<T> {
+    /* Because we don't need whole 8 bits, the first one is 
+     * used as a 'thread active' bit */
     thread_epochs: [AtomicU8; MAX_THREADS],
     global_epoch: AtomicU8,
 
@@ -14,18 +16,45 @@ pub struct Shared<T> {
 }
 
 impl<T> Shared<T> {
-    fn have_all_observed_epoch(&self, value: u8) -> bool {
-        for epoch in self.thread_epochs.iter().map(|x| x.load(Ordering::SeqCst)) {
-            if (epoch & 1) == 0 {
-                continue;
-            }
-            if (epoch >> 1) != value {
-                return false;
+    /// Returns the newest epoch and whether all threads have observed it
+    fn start_shared_section(&self, thread_id: usize) -> (u8, bool) {
+        let current_counter: u8 = self.global_epoch.load(Ordering::SeqCst);
+        let epoch = (current_counter << 1) | 1;
+        self.thread_epochs[thread_id].store(epoch, Ordering::SeqCst);
+
+        fence(Ordering::Acquire); // It's just nicer to have fresher data
+
+        for epoch in self.thread_epochs
+            .iter()
+            .map(|x| x.load(Ordering::Relaxed))
+            .filter(|epoch| (epoch & 1) != 0)
+        {
+            if (epoch >> 1) != current_counter {
+                return (current_counter, false);
             }
         }
 
-        self.global_epoch.fetch_add(1, Ordering::SeqCst);
-        return true;
+        /* bitwise AND this, just in case */
+        let next_counter = current_counter.wrapping_add(1) & 0b0111_1111;
+
+        /* TODO: maybe if succeeded, clean global garbage */
+        /* Many threads can try to increment at the same time, so it is
+         * important to use compare_exchange in this place */
+        let _ = self.global_epoch.compare_exchange(
+            current_counter,
+            next_counter,
+            Ordering::SeqCst,
+            Ordering::SeqCst
+        );
+
+        return (current_counter, true);
+    }
+
+    /// Returns the epoch that thread had while starting shared section
+    fn end_shared_section(&self, thread_id: usize) -> u8 {
+        let epoch = self.thread_epochs[thread_id].load(Ordering::Relaxed);
+        self.thread_epochs[thread_id].store(epoch & 0b1111_1110, Ordering::Release);
+        return epoch >> 1;
     }
 }
 
@@ -38,31 +67,30 @@ pub struct Local<T> {
 }
 
 impl<T> Local<T> {
-    /* Safety: `mark_use` must come in pair with `defer` */
+    /// Safety: `mark_use` must come in pair with `defer`
     pub unsafe fn mark_use(&mut self) {
-        let current_counter: u8 = self.shared.global_epoch.load(Ordering::SeqCst);
-        let epoch = (current_counter << 1) | 1;
-        self.shared.thread_epochs[self.thread_id].store(epoch, Ordering::SeqCst);
+        let (current_counter, have_threads_observed_epoch) = 
+            self.shared.start_shared_section(self.thread_id);
 
-        if self.shared.have_all_observed_epoch(current_counter) {
-            let two_ago = current_counter.wrapping_sub(2) % 4;
-            let two_ago = two_ago as usize;
-
-            /* Safety: since nobody uses these pointers, we can wrap them back into Box */
-            let iter = self.limbo[two_ago].iter()
-                .copied()
-                .map(|p| unsafe { Box::from_raw(p as *mut T) });
-
-            self.garbage.extend(iter);
-            self.limbo[two_ago].clear();
+        if !have_threads_observed_epoch {
+            return;
         }
+
+        let two_ago = current_counter.wrapping_sub(2) % 4;
+        let two_ago = two_ago as usize;
+
+        /* Safety: since nobody uses these pointers, we can wrap them back into Box */
+        let iter = self.limbo[two_ago].drain(..)
+            .map(|p| unsafe { Box::from_raw(p as *mut T) });
+
+        self.garbage.extend(iter);
     }
 
-    /* Safety: you can't defer the same pointer more than once,
-     * must come after `mark_use` */
+    /// Safety: you can't defer the same pointer more than once.
+    /// Must come after `mark_use`
     pub unsafe fn defer(&mut self, ptr: *const T) {
-        let epoch = self.shared.thread_epochs[self.thread_id].fetch_sub(1, Ordering::SeqCst);
-        let epoch = epoch >> 1;
+        let epoch = self.shared.end_shared_section(self.thread_id);
+        let epoch = epoch % 4;
         self.limbo[epoch as usize].push(ptr);
     }
 }
